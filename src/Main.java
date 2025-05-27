e06bbf2039fb46c7babdeaa83b776abdf517c0d7
Anselmo Ortega <lfkanselmo@gmail.com>
2025-05-27 10:20:29 -0500

    refactor the main is changed

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    String nombre="sebastian garzon villada";
+    int edad=23;
+
 }
